964d4b42d35f51eac9f007f2a00ae2e3322bf8d5
Shaun Alexander <shaun.alexander@aa.com>
2025-03-15 21:13:01 -0500

    Update README for Raspberry Pi OS upgrade instructions and modify image capture format to JPEG

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # autopilotai-pi
 
 ## Installation on Raspberry Pi Zero 2
+
+### Upgrade Raspberry Pi OS with latest packages
+```sh
+sudo apt update
+sudo apt upgrade
+```
+
+### Install application packages
 ```sh
 sudo apt install python3-fastapi
 sudo apt install python3-uvicorn
--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 import time
 from fastapi import FastAPI
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
 import uvicorn
 from io import BytesIO
 import base64
 
 camera = Picamera2()
-camera.start_preview(Preview.QTGL)
-config = camera.create_preview_configuration(main={"size": (800, 600)})
-# config = camera.create_still_configuration()
+# camera.start_preview(Preview.QTGL)
+# config = camera.create_preview_configuration(main={"size": (800, 600)})
+config = camera.create_still_configuration()
 camera.configure(config)
 
 app = FastAPI()
@@ -19,16 +19,13 @@ def capture_image():
         camera.start()
         time.sleep(2)
         # np_array = camera.capture_array()
-        camera.capture_file("image.png")
-        time.sleep(2)
+        camera.capture_file("image.jpeg")
         camera.stop()
 
-        # # Convert the numpy array to bytes
-        # image_bytes = np_array.tobytes()
-        # # Encode the bytes to base64
-        # image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+        with open("image.jpeg", "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
 
-        return {"image": "image_base64"}
+        return {"image": encoded_string}
     except Exception as e:
         return {"error": str(e)}
 
